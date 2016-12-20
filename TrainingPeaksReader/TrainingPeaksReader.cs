ab5ab179dea30e1ac46f51150de4de9d15785009
Eric Davis <eric.davis74@gmail.com>
2016-12-20 18:02:26 -0500

    Fixed CSV field parsing shortcomings.

--- TrainingPeaksReader/TrainingPeaksReader.cs
+++ TrainingPeaksReader/TrainingPeaksReader.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+
+using Microsoft.VisualBasic.FileIO;
 
 namespace FinalSurge
 {
@@ -9,34 +10,48 @@ namespace FinalSurge
     {
         public static List<Workout> ReadCsvFile(string csvFilePath, bool isFirstRowHeader = true)
         {
+            var records = new List<string[]>();
+            using (var parser = new TextFieldParser(csvFilePath) { Delimiters = new [] { "," }})
+            {
+                string[] fieldValues;
+                var isFirst = true;
+                do
+                {
+                    fieldValues = parser.ReadFields();
+                    if (fieldValues != null && (!isFirstRowHeader || !isFirst))
+                    {
+                        records.Add(fieldValues);
+                    }
+
+                    isFirst = false;
+                }
+                while (fieldValues != null);
+            }
+
             return
-                File.ReadAllLines(csvFilePath)
-                    .Skip(isFirstRowHeader ? 1 : 0)
-                    .Select(line => line.Split(new char[] { ',' }).Select(x => x.Replace("\"", string.Empty)).ToArray())
-                    .Select(
-                        x =>
-                            new Workout
-                                {
-                                    Type = x[0].Trim(),
-                                    Description = x[1].Trim(),
-                                    PlannedDuration = ParseDecimal(x[4]),
-                                    PlannedDistanceInMeters = ParseDecimal(x[5]),
-                                    WorkoutDate = ParseDateTime(x[6]),
-                                    CoachComments = x[7].Trim(),
-                                    DistanceInMeters = ParseDecimal(x[8]),
-                                    PowerAverage = ParseInt(x[9]),
-                                    PowerMax = ParseInt(x[10]),
-                                    Energy = ParseDecimal(x[11]),
-                                    AthleteComments = x[12].Trim(),
-                                    TotalTimeInHours = ParseDecimal(x[13]),
-                                    VelocityAverage = ParseDecimal(x[14]),
-                                    VelocityMax = ParseDecimal(x[15]),
-                                    CadenceAverage = ParseInt(x[16]),
-                                    CadenceMax = ParseInt(x[17]),
-                                    HeartRateAverage = ParseInt(x[18]),
-                                    HeartRateMax = ParseInt(x[19])
-                                })
-                    .ToList();
+                records.Select(
+                    x =>
+                        new Workout
+                            {
+                                Type = x[0].Trim(),
+                                Description = x[1].Trim(),
+                                PlannedDuration = ParseDecimal(x[4]),
+                                PlannedDistanceInMeters = ParseDecimal(x[5]),
+                                WorkoutDate = ParseDateTime(x[6]),
+                                CoachComments = x[7].Trim(),
+                                DistanceInMeters = ParseDecimal(x[8]),
+                                PowerAverage = ParseInt(x[9]),
+                                PowerMax = ParseInt(x[10]),
+                                Energy = ParseDecimal(x[11]),
+                                AthleteComments = x[12].Trim(),
+                                TotalTimeInHours = ParseDecimal(x[13]),
+                                VelocityAverage = ParseDecimal(x[14]),
+                                VelocityMax = ParseDecimal(x[15]),
+                                CadenceAverage = ParseInt(x[16]),
+                                CadenceMax = ParseInt(x[17]),
+                                HeartRateAverage = ParseInt(x[18]),
+                                HeartRateMax = ParseInt(x[19])
+                            }).ToList();
         }
 
         private static DateTime? ParseDateTime(string data)
